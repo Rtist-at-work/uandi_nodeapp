f5598fd3a1383d223de0713556ccd08b02d67d2d
Rtist-at-work <karthik18tech@gmail.com>
2024-11-30 16:57:36 +0530

    deploy ec2

--- server.js
+++ server.js
@@ -145,6 +145,6 @@ app.use('/upload-faqs', faq);
 
 // Start the server
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0',() => {
     console.log(`Server is running on port ${PORT}`);
 });
